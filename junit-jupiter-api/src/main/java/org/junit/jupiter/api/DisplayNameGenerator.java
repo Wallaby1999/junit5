47dabdd8da40389262e00880a3851430f046279f
Christian Stein <sormuras@gmail.com>
2018-09-16 16:15:06 +0200

    Introduce DisplayName generation SPI
    Addresses #162

---
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/DisplayNameGenerator.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.jupiter.api;
+
+import static org.apiguardian.api.API.Status.EXPERIMENTAL;
+
+import java.lang.reflect.Method;
+
+import org.apiguardian.api.API;
+
+/**
+ * {@code DisplayNameGenerator} defines the SPI for generating display
+ * names programmatically.
+ *
+ * @since 5.4
+ * @see DisplayName
+ * @see DisplayNameGeneration
+ */
+@API(status = EXPERIMENTAL, since = "5.4")
+public interface DisplayNameGenerator {
+
+	/**
+	 * Generate a display name for the given top-level test class.
+	 *
+	 * TODO Javadoc
+	 */
+	String generateDisplayNameForClass(Class<?> testClass);
+
+	/**
+	 * Generate a display name for the given nested test class.
+	 *
+	 * TODO Javadoc
+	 * TODO Find better name, split into two different methods?
+	 */
+	String generateDisplayNameForNestedClass(Class<?> nestedClass);
+
+	/**
+	 * Generate a display name for the given method.
+	 *
+	 * TODO Javadoc
+	 *
+	 * @implNote The class instance passed as {@code testClass} may differ from
+	 * the returned class by {@code testMethod.getDeclaringClass()}: e.g., when
+	 * a test method is inherited from a super class.
+	 *
+	 * @param testClass current test class this method "belongs" to
+	 * @param testMethod method to generate a display name for
+	 */
+	String generateDisplayNameForMethod(Class<?> testClass, Method testMethod);
+}
